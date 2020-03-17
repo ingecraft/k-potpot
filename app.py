c113561f481f64f2b756d309f8eb1b8241d359f7
ingecraft <ingenious.craftsman@gmail.com>
2020-03-17 20:14:57 +0200

    Created realworld templates

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello():
-    return render_template('test_index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
