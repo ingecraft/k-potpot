49e1b5c2f649ef0b54309d0297c3ebffdace9de6
ingecraft <ingenious.craftsman@gmail.com>
2020-03-20 16:40:07 +0200

    Added more test elements

--- static/js/app.js
+++ static/js/app.js
@@ -23,3 +23,18 @@ var polygon = L.polygon([
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
