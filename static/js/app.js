e2980ab2bd1d6de408c9e9d2bccb2555a750330f
ingecraft <ingenious.craftsman@gmail.com>
2020-03-20 16:08:56 +0200

    Added polygon to index map

--- static/js/app.js
+++ static/js/app.js
@@ -18,5 +18,8 @@ var circle = L.circle([51.508, -0.11], {
 	radius: 500
 }).addTo(mymap);
 
-	
-
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
